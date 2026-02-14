8057f89fc08e22a466ec3b288a6a0163fb344b2b
londonfogknox-maker <londonfogknox@gmail.com>
2026-02-14 17:12:41 +0000

    Pending changes exported from your codespace

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -2,6 +2,30 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "title": "Django example",
+        "title": "Periwinkle Punk Offical",
     }
     return render(request, "index.html", context)
+
+def mybooks(request):
+    context = {
+        "title": "My Books",
+    }
+    return render(request, "mybooks.html", context)
+
+def moodboard(request):
+    context = {
+        "title": "Mood Board",
+    }
+    return render(request, "moodboard.html", context)
+
+def updates(request):
+    context = {
+        "title": "Author's Note",
+    }
+    return render(request, "updates.html", context)
+
+def about(request):
+    context = {
+        "title": "About the Author",
+    }
+    return render(request, "about.html", context)
